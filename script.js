d773ace529c5d5b877b2b6fb4edec9cb1446c5b1
Samuel <sumlebulle@hotmail.com>
2018-11-29 11:42:07 +0100

    Almost everything

--- script.js
+++ script.js
@@ -1,51 +1,123 @@
 const cards = [
-  { name: 'Purple', color: '#77567A', image: './images/Face_With_Rolling_Eyes_Emoji.png'},
-  { name: 'Green', color: '#68A357', image: './images/Flushed_Face_Emoji.png' },
-  { name: 'Pink', color: '#F79AD3', image: './images/Hungry_Face_Emoji.png' },
-  { name: 'Blue', color: '#2F97C1', image: './images/Smiling_Emoji_with_Eyes_Opened.png' },
-  { name: 'Red', color: '#F71735', image: './images/Sunglasses_Emoji.png' },
-  { name: 'Orange', color: '#FF9F1C', image: './images/Tears_of_Joy_Emoji.png' },
-  { name: 'White', color: '#FDFFFC', image: './images/Tongue_Out_Emoji_with_Winking_Eye.png' },
-  { name: 'Lime', color: '#DBFE87', image: './images/Very_Angry_Emoji.png' },
-  { name: 'Purple', color: '#77567A', image: './images/Face_With_Rolling_Eyes_Emoji.png'},
-  { name: 'Green', color: '#68A357', image: './images/Flushed_Face_Emoji.png' },
-  { name: 'Pink', color: '#F79AD3', image: './images/Hungry_Face_Emoji.png' },
-  { name: 'Blue', color: '#2F97C1', image: './images/Smiling_Emoji_with_Eyes_Opened.png' },
-  { name: 'Red', color: '#F71735', image: './images/Sunglasses_Emoji.png' },
-  { name: 'Orange', color: '#FF9F1C', image: './images/Tears_of_Joy_Emoji.png' },
-  { name: 'White', color: '#FDFFFC', image: './images/Tongue_Out_Emoji_with_Winking_Eye.png' },
-  { name: 'Lime', color: '#DBFE87', image: './images/Very_Angry_Emoji.png' }
+  { id: 1, image: './assets/Face_With_Rolling_Eyes_Emoji.png'},
+  { id: 2, image: './assets/Flushed_Face_Emoji.png' },
+  { id: 3, image: './assets/Hungry_Face_Emoji.png' },
+  { id: 4, image: './assets/Smiling_Emoji_with_Eyes_Opened.png' },
+  { id: 5, image: './assets/Sunglasses_Emoji.png' },
+  { id: 6, image: './assets/Tears_of_Joy_Emoji.png' },
+  { id: 7, image: './assets/Tongue_Out_Emoji_with_Winking_Eye.png' },
+  { id: 8, image: './assets/Very_Angry_Emoji.png' },
+  { id: 1, image: './assets/Face_With_Rolling_Eyes_Emoji.png'},
+  { id: 2, image: './assets/Flushed_Face_Emoji.png' },
+  { id: 3, image: './assets/Hungry_Face_Emoji.png' },
+  { id: 4, image: './assets/Smiling_Emoji_with_Eyes_Opened.png' },
+  { id: 5, image: './assets/Sunglasses_Emoji.png' },
+  { id: 6, image: './assets/Tears_of_Joy_Emoji.png' },
+  { id: 7, image: './assets/Tongue_Out_Emoji_with_Winking_Eye.png' },
+  { id: 8, image: './assets/Very_Angry_Emoji.png' }
 ]
-// Shuffle the array:
-function compareRandom(a, b) {
-  return Math.random() - 0.5 ;
-}
 
-cards.sort(compareRandom);
-// End shuffle
-
+const startBtn = document.querySelector('.startBtn')
+const greeting = document.querySelector('.greeting')
 const container = document.querySelector('.container')
+const finished = document.querySelector('.finished-box')
+const restartBtn = document.querySelector('.restartBtn')
 
-function createCard(image, color){
-  const card = `
-  <div class="card" data-color="${color}">
-   <img src="${image}">
-  </div>
-  `
-  container.innerHTML += card
+let audio = new Audio('./assets/whistle-stop.mp3')
+
+let solvedCards = []
+let toggledCards = []
+let animating = false;
+
+startBtn.addEventListener('click', () => {
+  startGame()
+  container.classList.remove('hidden')
+  greeting.style.display = "none"
+})
+
+restartBtn.addEventListener('click', () => {
+  finished.style.display = "none";
+  restartGame()
+})
+
+  // Shuffle the array:
+function compareRandom(a, b) {
+  return Math.random() - 0.5
 }
 
-cards.forEach((value) => {
-  createCard(value.image, value.color,)
-});
 
-let displayCard = function (){
-  this.classList.toggle("open");
-  this.classList.toggle("show");
-  this.classList.toggle("disabled");
+function restartGame() {
+  solvedCards = []
+  container.innerHTML = ''
+  startGame()
 }
 
-const things = document.querySelectorAll('.card')
-for (let i = 0; i < things.length; i++) {
-  things[i].addEventListener('click', displayCard);
+function startGame() {
+
+  audio.loop = true
+  audio.play()
+
+  cards.sort(compareRandom);
+
+  // Print the cards array
+
+  const container = document.querySelector('.container')
+
+  function createCard(id, image){
+    const card = `
+    <div data-id="${id}" class="card toggle-off">
+      <div class="back"><img src="${image}"></div>
+      <div class="front"></div>
+    </div>
+    `
+    container.innerHTML += card
+  }
+
+  for (let i = 0; i < cards.length; i++) {
+    createCard(cards[i].id, cards[i].image)
+  }
+  // End Print the cards array
+
+  // Turn one card
+  const cardElements = document.querySelectorAll('.container .card')
+  for (let i = 0; i < cardElements.length; i++) {
+    cardElements[i].addEventListener('click', () => {
+
+      if (!animating) {
+        if(!cardElements[i].classList.contains('toggle-on')){
+
+          cardElements[i].classList.add('toggle-on')
+          toggledCards.push(cardElements[i])
+
+
+          if(toggledCards.length >= 2){
+            if (toggledCards[0].getAttribute('data-id') === toggledCards[1].getAttribute('data-id')) {
+              console.log('match');
+              solvedCards = solvedCards.concat(toggledCards)
+
+              if (solvedCards.length === 16) {
+                finished.style.display = "flex";
+                audio.pause()
+                audio.currentTime = 0.0
+              }
+
+              toggledCards = []
+            } else {
+              animating = true;
+              setTimeout(() => {
+                toggledCards.forEach((card) => {
+                  card.classList.remove('toggle-on')
+                })
+                toggledCards = []
+                animating = false;
+              }, 1300)
+            }
+          }
+        }else{
+          console.log('This card is already active')
+        }
+      }
+    })
+  }
 }
+// End Turn one card
